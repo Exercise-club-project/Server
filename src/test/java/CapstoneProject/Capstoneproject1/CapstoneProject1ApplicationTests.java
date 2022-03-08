a6be440cc2619ba2742aa5daaddcb5e8b83e6fb2
Kwon <pawoo0211@naver.com>
2022-03-08 17:15:29 +0900

    initial commit

---
+++ src/test/java/CapstoneProject/Capstoneproject1/CapstoneProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package CapstoneProject.Capstoneproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
