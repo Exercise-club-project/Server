36ace9a0dc182e6d1959c1cb2acabbec11f0a90f
pawoo0211 <pawoo0211@naver.com>
2022-05-31 21:52:28 +0900

    refactor: 모임 상세내용 API 수정

--- src/main/java/CapstoneProject/Capstoneproject1/domain/meeting/domain/MeetingUserRepository.java
+++ src/main/java/CapstoneProject/Capstoneproject1/domain/meeting/domain/MeetingUserRepository.java
@@ -8,5 +8,6 @@ import java.util.List;
 public interface MeetingUserRepository extends JpaRepository<MeetingUser,Long> {
     MeetingUser findByMeeting(Long meeting);
     List<MeetingUser> findAllByUser(User user);
-    List<User> findAllByMeeting(Long meetingId);
+    List<MeetingUser> findAllByMeeting(Meeting meeting);
+    /* By~는 자바 클래스에 있는 속성 값을 인자로 받아서 찾는 것, 즉 By~(인자) 인자에 속성값이 들어와야 함 */
 }
